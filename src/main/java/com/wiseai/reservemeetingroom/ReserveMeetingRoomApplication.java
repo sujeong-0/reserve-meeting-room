dc311d46670ea013b9ab57148491d3c477b377da
공수정 <brightelf9@naver.com>
2025-04-30 19:49:13 +0900

    chore: init project

---
+++ src/main/java/com/wiseai/reservemeetingroom/ReserveMeetingRoomApplication.java
@@ -0,0 +1,13 @@
+package com.wiseai.reservemeetingroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveMeetingRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserveMeetingRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wiseai/reservemeetingroom/ReserveMeetingRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiseai.reservemeetingroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveMeetingRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
