4daa0ed0043c560145c45560160d5e847494bc3f
공수정 <brightelf9@naver.com>
2025-04-30 23:26:24 +0900

    feat: user 조회 swagger 작성 #1

---
+++ src/main/java/com/wiseai/reservemeetingroom/user/api/swagger/UserSwagger.java
@@ -0,0 +1,60 @@
+package com.wiseai.reservemeetingroom.user.api.swagger;
+
+import com.wiseai.reservemeetingroom.core.domain.UrlPath.User;
+import com.wiseai.reservemeetingroom.user.api.response.UserResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 이 클래스는 유저 관련 문서를 담당합니다.
+ */
+
+@Tag(
+	name = "User API",
+	description = """
+		<b>유저 관련 API입니다.</b><br><br>
+		"""
+)
+@RequestMapping(User.ROOT)
+public interface UserSwagger {
+
+	/*──────────────────────────────────────────────────────
+	 * 1. 유저 조회
+	 *──────────────────────────────────────────────────────*/
+	@Operation(
+		summary = "User 조회",
+		description = """
+			email로 유저를 조회합니다.
+			""",
+		responses = {
+			@ApiResponse(
+				responseCode = "200",
+				description = "유저 조회 성공",
+				content = @Content(
+					schema = @Schema(implementation = UserResponse.class)
+					/*examples = @ExampleObject(
+						value = """
+                        {
+                          "accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
+                          "refreshToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+                          "isOnboardingCompleted": false
+                        }"""
+					)*/
+				)
+			)
+		}
+	)
+	@PostMapping("/login/oauth")
+	UserResponse loginWithOAuth(
+		@Parameter(description = "유저 이메일", example = "test@email.com", required = true)
+		@RequestParam("email") String email
+	);
+
+}
