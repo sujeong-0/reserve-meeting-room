6a163515c57a0ae27d934e91b52c45a2456ce964
공수정 <brightelf9@naver.com>
2025-05-04 01:40:43 +0900

    feat: 회의실 엔티티 및 repository 정의

---
+++ src/main/java/com/wiseai/reservemeetingroom/meeting_room/domain/MeetingRoom.java
@@ -0,0 +1,67 @@
+package com.wiseai.reservemeetingroom.meeting_room.domain;
+
+import com.wiseai.reservemeetingroom.core.domain.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 이 클래스는 회의실 엔티티를 담당합니다.
+ */
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "meeting_rooms")
+public class MeetingRoom extends BaseTimeEntity {
+
+	/**
+	 * 회의실 아이디
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	/**
+	 * 이름
+	 */
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	/**
+	 * 위치
+	 */
+	@Column(name = "location", nullable = false, unique = true)
+	private String location;
+
+	/**
+	 * 삭제 여부
+	 */
+	@Column(name = "is_deleted", nullable = false)
+	@Builder.Default
+	private boolean deleted = false;
+
+	public void updateFrom(MeetingRoom source) {
+		if (source.getName() != null) {
+			this.name = source.getName();
+		}
+		if (source.getLocation() != null) {
+			this.location = source.getLocation();
+		}
+	}
+
+
+	public void markAsDeleted() {
+		this.deleted = true;
+	}
+
+}
---
+++ src/main/java/com/wiseai/reservemeetingroom/meeting_room/domain/repository/MeetingRoomRepository.java
@@ -0,0 +1,11 @@
+package com.wiseai.reservemeetingroom.meeting_room.domain.repository;
+
+import com.wiseai.reservemeetingroom.meeting_room.domain.MeetingRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * 이 클래스는 회의실 저장소를 담당합니다.
+ */
+public interface MeetingRoomRepository extends JpaRepository<MeetingRoom, Long> {
+
+}
