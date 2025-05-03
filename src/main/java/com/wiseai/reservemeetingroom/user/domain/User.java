a2e35728bfde0fe1cb1c6b8d60268eef6cf8ba77
공수정 <brightelf9@naver.com>
2025-05-03 21:20:27 +0900

    chore: README 작성

--- src/main/java/com/wiseai/reservemeetingroom/user/domain/User.java
+++ src/main/java/com/wiseai/reservemeetingroom/user/domain/User.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,6 +22,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "users")
 public class User extends BaseTimeEntity {
 
 	/** 유저 아이디 */
